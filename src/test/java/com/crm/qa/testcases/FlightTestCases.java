1d4721458369f5f78f1ff775bb388f008cf49dc7
dhananjaya2007 <dhananjaya2007@hotmail.com>
2023-06-28 10:12:08 -0400

    Adding a comment to show the class

--- src/test/java/com/crm/qa/testcases/FlightTestCases.java
+++ src/test/java/com/crm/qa/testcases/FlightTestCases.java
@@ -25,6 +25,7 @@ public class FlightTestCases extends TestBase {
 		homepage = new HomePage();
 	}
 
+	//Test comment added by dan
 	@Test(groups = { "Simple", "UI" })
 	public void TC001_CheckifLogoisPresent() {
 		boolean flag = homepage.isLogoDisplayed();
